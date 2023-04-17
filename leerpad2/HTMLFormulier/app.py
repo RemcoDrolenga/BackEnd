a49a3726003ef9f7c812faf560fbbd7955223d01
Remco Drolenga <89783539+RemcoDrolenga@users.noreply.github.com>
2023-04-17 10:37:55 +0200

    opdracht gemaakt

--- leerpad2/HTMLFormulier/app.py
+++ leerpad2/HTMLFormulier/app.py
@@ -8,9 +8,9 @@ def hello_world():
     return render_template("welcome.html")
 
 
-@app.route("/submit")
+@app.route("/submit", methods=["POST"])
 def submit():
-    name = request.args.get("name")
-    email = request.args.get("email")
+    name = request.form.get("name")
+    email = request.form.get("email")
     print(name, email)
     return "<h1>De ingevulde gegevens zijn:</h1> <br> <p>Naam: " + name + "</p> <br> <p>Email: " + email + "</p>"
